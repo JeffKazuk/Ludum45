1d838dc7c5145be06da4101af962a33def2074a7
JeffKazuk <32557408+JeffKazuk@users.noreply.github.com>
2019-10-06 16:49:18 -0500

    Can enter chef shack

--- Scripts/Town/MailBox.gd
+++ Scripts/Town/MailBox.gd
@@ -1,9 +1,7 @@
 extends Spatial
-var GlobalVars
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(self.get_name())
-	GlobalVars = get_node("/root/GlobalVars")
 
 func _on_interact():
 	if GlobalVars.mail_started:
