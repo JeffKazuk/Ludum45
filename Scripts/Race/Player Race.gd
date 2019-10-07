0ba1f9b13a89df18c86568603331ca7aa61315aa
JeffKazuk <32557408+JeffKazuk@users.noreply.github.com>
2019-10-07 08:39:49 -0500

    Added the big race
    4/5 bahbey

--- Scripts/Race/Player Race.gd
+++ Scripts/Race/Player Race.gd
@@ -1,18 +1,56 @@
 extends AnimatedSprite3D
 
+var dialogue = preload("res://dialogue/Dialogue.tscn")
+var ins = dialogue.instance()
+
+var started = false
 var left = true
 signal race_start
+var run_frame = 0
+var runner
+var result = ""
+var finished = false
 
 func _ready():
-    self.connect("race_start", get_parent().get_node("Runner Race"), "_race_started")
+    runner = get_parent().get_node("Runner Race")
+    self.connect("race_start", runner, "_race_started")
+    GlobalVars.race_attempted = true
+
+func _process(delta):
+
+    if runner.finished:
+        if not result:
+            result = "loss"
+
+    if self.translation.x <= -26:
+        finished = true
+        animation = "waiting"
+        if not result:
+            result = "won"
+        if runner.finished:
+            if result == "won":
+                GlobalVars.race_done = true
+                ins.file = "jogger2"
+                add_child(ins)
+            elif result == "loss":
+                ins.file = "joggerlose"
+                add_child(ins)
+        
+
+func start_race():
+        emit_signal("race_start")
+        started = true
+        animation = "running"
 
 func _input(event):
-    if event.is_action_pressed("strafe_left"):
-        emit_signal("race_start")
-        if left == true:
-            translation.x -= 0.1
-            left = false
-    if event.is_action_pressed("strafe_right"):
-        if left == false:
-            translation.x -= 0.1
-            left = true
+    if not finished:
+        if event.is_action_pressed("ui_left"):
+            if not started:
+                start_race()
+            if left == true:
+                translation.x -= .15
+                left = false
+        if event.is_action_pressed("ui_right"):
+            if left == false:
+                translation.x -= .15
+                left = true
--- Scripts/Race/Runner Race.gd
+++ Scripts/Race/Runner Race.gd
@@ -1,12 +1,17 @@
 extends AnimatedSprite3D
-var distance = 30
+var finish = -26
+var finished = false
 export var running = false
 
 func _process(delta):
 	if running:
-		self.translation.x = -((1-$Timer.time_left/$Timer.wait_time)*distance)
+		self.translation.x = ($Timer.time_left/$Timer.wait_time)*49.29-26
+	if self.translation.x <= -26:
+		animation = "waiting"
+		finished = true
+		running = false
 
 func _race_started():
-	if running == false:
-		running = true
-		$Timer.start()
+	animation = "running"
+	running = true
+	$Timer.start()
