94703d88d33174d8390579f80069f7a719b39890
Shane <shanescott86@gmail.com>
2019-10-07 05:41:38 -0500

    sadfasdf

--- Scripts/Grocery/Jerry.gd
+++ Scripts/Grocery/Jerry.gd
@@ -10,10 +10,10 @@ func _on_interact():
 	#First TIme
 	if GlobalVars.grocery_attempted == false:
 		GlobalVars.grocery_attempted = true
-		ins.file = "Grocer PLACEHOLDER"
+		ins.file = "grocer2"
 		add_child(ins)
 	#Rematch
 	elif GlobalVars.grocery_attempted == true && GlobalVars.grocery_done == false:
 		GlobalVars.mail_done = true
-		ins.file = "Grocer Rematch PLACEHOLDER"
+		ins.file = "grocerrematch"
 		add_child(ins)
